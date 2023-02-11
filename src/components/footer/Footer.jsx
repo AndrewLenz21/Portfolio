02c4a8bc7cb11e2230511b11a7e5eae9b3be643d
AndrewLenz21 <angelitolenz2000@gmail.com>
2023-02-11 11:35:45 +0100

    Adding ` rel="noreferrer"` on links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,19 +10,19 @@ const Footer = () => {
         <div className="social__footer">
             <div className="icons__footer">
                 {/* Github link */}
-                <a href="https://bit.ly/githublenz" className="home__social-icon" target="_blank">
+                <a href="https://bit.ly/githublenz" className="home__social-icon" target="_blank" rel="noreferrer">
                     <i class="uil uil-github-alt"></i>
                 </a>
                 {/* Linkedin link */}
-                <a href="https://bit.ly/linkedinlenzprofile" className="home__social-icon" target="_blank">
+                <a href="https://bit.ly/linkedinlenzprofile" className="home__social-icon" target="_blank" rel="noreferrer">
                     <i class="uil uil-linkedin"></i>
                 </a>
                 {/* Instagram link */}
-                <a href="https://bit.ly/instagramlenz" className="home__social-icon" target="_blank">
+                <a href="https://bit.ly/instagramlenz" className="home__social-icon" target="_blank" rel="noreferrer">
                    <i class="uil uil-instagram"></i>
                 </a>
                 {/* Codepen link */}
-                <a href="https://bit.ly/codepenLenz" className="home__social-icon" target="_blank">
+                <a href="https://bit.ly/codepenLenz" className="home__social-icon" target="_blank" rel="noreferrer">
                     <img src={images.logoCodePen} alt="logoCodePen" />
                 </a>
             </div>
